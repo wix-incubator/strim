022ff89cfc9615a12db6b499df69d9b9d5a27076
Dean Shub <deanshub@gmail.com>
2019-03-04 11:18:36 +0200

    adding async await support

--- babel.config.js
+++ babel.config.js
@@ -4,5 +4,6 @@ module.exports = {
     '@babel/proposal-class-properties',
     '@babel/proposal-object-rest-spread',
     '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-transform-runtime',
   ],
 }
