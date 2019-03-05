395de65155810d3bf3ab480b2100395ff949025e
Dean Shub <deanshub@gmail.com>
2019-03-05 15:19:03 +0200

    adding webpack bundling

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+let webpack = require('webpack')
+let path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+const clientConfig = {
+  entry: './src/Strim/Strim.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'strim.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader?cacheDirectory=true'],
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', 'tsx', '.json'],
+  },
+}
+
+const serverConfig = {
+  entry: './src/strimModules/strimModules.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'strimModules.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader?cacheDirectory=true'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', 'tsx', '.json'],
+  },
+}
+
+module.exports = [clientConfig, serverConfig]
